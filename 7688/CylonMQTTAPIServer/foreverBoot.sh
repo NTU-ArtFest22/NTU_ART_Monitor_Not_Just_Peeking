b18ef5b540d3b987aa7e6d4347fb776e7bb51db3
Tsukino KanTei <r03944003@ntu.edu.tw>
2016-05-05 22:48:08 -0700

    add logging feature to foreverBoot.sh

--- 7688/CylonMQTTAPIServer/foreverBoot.sh
+++ 7688/CylonMQTTAPIServer/foreverBoot.sh
@@ -1,34 +1,16 @@
 START=93
 
-# USE_PROCD=1
-# PROG="$APIServerPath"/node_modules/forever/bin/forever
-
 start() {        
   echo "forever started"
   export PATH=$PATH:"$APIServerPath"/node_modules/forever/bin
-  exec forever start --sourceDir=$APIServerPath app.js
+  exec forever start --sourceDir=$APIServerPath -l "$APIServerPath"/out.log -o "$APIServerPath"/appOut.log -e "$APIServerPath"/appErr.log --uid "NJP" app.js
 }                 
 
 stop() {          
   echo "forever stopped"
   export PATH=$PATH:"$APIServerPath"/node_modules/forever/bin
-  exec forever stop --sourceDir=$APIServerPath app.js
+  exec forever stop NJP
 }
-
-# start_instance() {
-#   procd_open_instance
-#   procd_set_param command "$PROG" --input "start --sourceDir=$APIServerPath app.js"
-#   procd_close_instance
-# }
-
-# start_service() {
-#   echo "forever service start"
-#   config_foreach start_instance 'foreverBoot'
-# }
-
-# service_triggers() {
-#   procd_add_reload_trigger 'foreverBoot'
-# }
 
 boot() {
   echo "forever boot up"
