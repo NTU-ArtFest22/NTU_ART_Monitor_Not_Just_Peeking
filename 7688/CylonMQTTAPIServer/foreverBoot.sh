06a51e35d7a2ce6f374efc7ff20239c9cdd32d26
Tsukino KanTei <r03944003@ntu.edu.tw>
2016-05-06 08:38:39 -0700

    fix some bugs in foreverBoot.sh

--- 7688/CylonMQTTAPIServer/foreverBoot.sh
+++ 7688/CylonMQTTAPIServer/foreverBoot.sh
@@ -3,13 +3,19 @@ START=93
 start() {        
   echo "forever started"
   export PATH=$PATH:"$APIServerPath"/node_modules/forever/bin
-  exec forever start --sourceDir=$APIServerPath -l "$APIServerPath"/out.log -o "$APIServerPath"/appOut.log -e "$APIServerPath"/appErr.log --uid "NJP" app.js
+  exec forever start --sourceDir=$APIServerPath -a -l "$APIServerPath"/out.log -o "$APIServerPath"/appOut.log -e "$APIServerPath"/appErr.log --uid "NJP" app.js
 }                 
 
 stop() {          
   echo "forever stopped"
   export PATH=$PATH:"$APIServerPath"/node_modules/forever/bin
   exec forever stop NJP
+}
+
+restart() {
+  echo "to restart"
+  stop
+  start
 }
 
 boot() {
